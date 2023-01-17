3fc6c521192cfd798ca840082e54149df4877fbe
Robert James Kaes <rjk@wormbytes.ca>
2023-01-17 16:14:56 -0500

    Complete Conversion of Globals to Lua
    Only item left is table for `dispatch_compilers`.

--- lua/custom/globals.lua
+++ lua/custom/globals.lua
@@ -17,20 +17,22 @@ vim.g.tada_todo_style = 'unicode'
 vim.g.tada_todo_pane_file = 'TODO'
 vim.g.tada_todo_pane_location = 'top'
 
-vim.cmd [[
-let g:ruby_host_prog = expand('$HOME/.rubies/ruby-3.1.3/bin/ruby')
-let g:ruby_path = g:ruby_host_prog
+vim.g['semshi#always_update_all_highlights'] = true
 
-let g:test#preserve_screen = 0
-let test#strategy = 'dispatch'
+-- Ruby testing
+vim.g.ruby_host_prog = vim.fn.expand('$HOME/.rubies/ruby-3.1.3/bin/ruby')
+vim.g.ruby_path = vim.g.ruby_host_prog
 
-" Run rspec (with bundle exec) but use the "rails" compiler to parse the output
-let g:test#ruby#bundle_exec = 1
+vim.g['test#preserve_screen'] = 0
+vim.v['test#strategy'] = 'dispatch'
+
+-- Run rspec (with bundle exec) but use the "rails" compiler to parse the output
+vim.g['test#ruby#bundle_exec'] = 1
+
+-- TODO: Figure out how to manipulate these tables with lua
+vim.cmd[[
 if !exists('g:dispatch_compilers')
   let g:dispatch_compilers = {}
 endif
 let g:dispatch_compilers['bundle exec '] = ''
-" let g:dispatch_compilers['rspec'] = 'rake'
-
-let g:semshi#always_update_all_highlights = v:true
 ]]
