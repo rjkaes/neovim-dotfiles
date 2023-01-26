d001c16538824a5c19890bbb26c1e2d1ca777432
Robert James Kaes <rjk@wormbytes.ca>
2023-01-26 11:44:47 -0500

    Remove AC/Battery Check for COQ
    The M2 Pro has so much headroom on battery that there's no point in
    artificially lowering performance.

--- lua/custom/plugins/coq.lua
+++ lua/custom/plugins/coq.lua
@@ -3,17 +3,10 @@ return {
         'ms-jpq/coq_nvim',
         branch = 'coq',
         init = function()
-            -- If we're connected to AC, then perform full autocompletion; otherwise, don't.
-            -- Completion takes up a bunch of power that isn't available on battery.
-            local always_completion = true
-            if vim.fn.executable('pmset') == 1 then
-                always_completion = not(vim.fn.system('pmset -g batt'):find('AC Power', 1, true) == nil)
-            end
-
             vim.g.coq_settings = {
                 auto_start = 'shut-up',
                 completion = {
-                    always = always_completion,
+                    always = true,
                 },
                 keymap = {
                     recommended = false,
