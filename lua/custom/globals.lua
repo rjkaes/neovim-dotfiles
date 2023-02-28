344539f6b080952430f6322c7e5dc74c17ddfa53
Robert James Kaes <rjk@wormbytes.ca>
2023-02-28 14:36:12 -0500

    fix: upgrade to ruby 3.2.1

--- lua/custom/globals.lua
+++ lua/custom/globals.lua
@@ -19,7 +19,7 @@ vim.g.tada_todo_pane_location = 'top'
 vim.g['semshi#always_update_all_highlights'] = true
 
 -- Ruby testing
-vim.g.ruby_host_prog = vim.fn.expand('$HOME/.rubies/ruby-3.2.0/bin/ruby')
+vim.g.ruby_host_prog = vim.fn.expand('$HOME/.rubies/ruby-3.2.1/bin/ruby')
 vim.g.ruby_path = vim.g.ruby_host_prog
 
 vim.g['test#preserve_screen'] = 0
