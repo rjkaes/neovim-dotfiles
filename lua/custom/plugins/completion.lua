cabb27cd8f47be2a1b34f22de405e4b984f7afa1
Robert James Kaes <rjk@wormbytes.ca>
2023-03-11 17:14:39 -0500

    feat(completion): remove cmp-path and add lspkind
    Completing paths served no purpose.  But adding LSP symbols to the
    completion popup is a nice to have.

--- lua/custom/plugins/completion.lua
+++ lua/custom/plugins/completion.lua
@@ -1,13 +1,27 @@
 return {
     { 'hrsh7th/cmp-nvim-lsp', branch = 'main' },
     { 'hrsh7th/cmp-buffer', branch = 'main' },
-    { 'hrsh7th/cmp-path', branch = 'main' },
+    { 'hrsh7th/cmp-cmdline', branch = 'main' },
+    { 'onsails/lspkind.nvim' },
     {
         'hrsh7th/nvim-cmp',
         branch = 'main',
         config = function()
-            local cmp = require'cmp'
+            vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+
+            local lspkind = require('lspkind')
+            local cmp = require('cmp')
             cmp.setup({
+                performance = {
+                    debounce = 100,
+                },
+                formatting = {
+                    format = lspkind.cmp_format({
+                        mode = 'symbol', -- show only symbol annotations
+                        maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                        ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                    })
+                },
                 snippet = {
                     -- REQUIRED by nvim-cmp. get rid of it once we can
                     expand = function(args)
@@ -19,7 +33,7 @@ return {
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
                     ['<Tab>'] = function(fallback)
                         if cmp.visible() then
                             cmp.select_next_item()
@@ -35,22 +49,42 @@ return {
                         end
                     end,
                 }),
-                sources = cmp.config.sources({
-                    -- TODO: currently snippets from lsp end up getting prioritized -- stop that!
-                    { name = 'nvim_lsp' },
-                    { name = 'buffer' },
-                }, {
-                        { name = 'path' },
+                sources = cmp.config.sources(
+                    {
+                        -- TODO: currently snippets from lsp end up getting prioritized -- stop that!
+                        { name = 'nvim_lsp' },
+                    },
+                    {
+                        {
+                            name = 'buffer',
+                            option = {
+                                get_bufnrs = function()
+                                    return vim.api.nvim_list_bufs()
+                                end
+                            },
+                        },
                     }),
                 experimental = {
                     ghost_text = true,
                 },
             })
 
-            -- Enable completing paths in :
+            cmp.setup.cmdline('/', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = 'buffer' }
+                }
+            })
+
             cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
                 sources = cmp.config.sources({
-                    { name = 'path' }
+                    {
+                        name = 'cmdline',
+                        option = {
+                            ignore_cmds = { 'Man', '!' }
+                        }
+                    }
                 })
             })
 
