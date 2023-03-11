308182270d17b60f15ff1e50935f94c9fcf43825
Robert James Kaes <rjk@wormbytes.ca>
2023-03-11 17:13:27 -0500

    feat(gitsigns): use default highlight groups and styles

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -3,13 +3,6 @@ return {
     {
         'lewis6991/gitsigns.nvim',
         opts = {
-            signs = {
-                add          = {hl = 'GitSignsAdd'   , text = '+', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-                change       = {hl = 'GitSignsChange', text = '!', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-                delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-                topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-                changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-            },
             current_line_blame = false,
             on_attach = function(bufnr)
                 local function map(mode, lhs, rhs, opts)
