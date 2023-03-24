76312fa7a4f815dfcf9816a6c96273ab613bf207
Robert James Kaes <rjk@wormbytes.ca>
2023-03-24 13:12:04 -0400

    feat: configure lua-language-server defaults

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -7,9 +7,21 @@ local setup_lua_ls = function(capabilities)
         on_attach = on_attach,
         settings = {
             Lua = {
+                runtime = {
+                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                    version = 'LuaJIT',
+                },
                 diagnostics = {
                     -- Get the language server to recognize the `vim` global
                     globals = {'vim'},
+                },
+                workspace = {
+                    -- Make the server aware of Neovim runtime files
+                    library = vim.api.nvim_get_runtime_file("", true),
+                },
+                -- Do not send telemetry data containing a randomized but unique identifier
+                telemetry = {
+                    enable = false,
                 },
             },
         }
@@ -29,7 +41,7 @@ return {
                     local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
                     lsp.setup({
-                        ensure_installed = { 'lua_ls' },
+                        ensure_installed = { 'lua_ls', 'rust_analyzer' },
                         automatic_installation = true,
                     })
 
