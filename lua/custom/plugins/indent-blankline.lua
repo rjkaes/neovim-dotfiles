73233c41ac339c225c093bb91d3227885243266e
Robert James Kaes <rjk@wormbytes.ca>
2023-09-28 09:08:55 -0400

    chore: upgrade indent-blankline config for v3

--- lua/custom/plugins/indent-blankline.lua
+++ lua/custom/plugins/indent-blankline.lua
@@ -2,12 +2,7 @@
 return {
     {
         'lukas-reineke/indent-blankline.nvim',
-        opts = {
-            show_first_indent_level = false,
-            space_char_blankline = " ",
-            show_current_context = false,
-            filetype_exclude = {"help"},
-            buftype_exclude = {"terminal"},
-        },
+        main = 'ibl',
+        opts = {},
     },
 }
