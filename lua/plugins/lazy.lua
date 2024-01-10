22ed5c4a11da02f1b457fd0ed00ae961d324b3dd
Robert James Kaes <robert@kaes.ca>
2024-01-10 12:17:29 -0500

    feat(plugin): add harpoon

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -161,6 +161,21 @@ require("lazy").setup({
     -- { 'cweagans/vim-taskpaper'
     { 'dewyze/vim-tada' },
 
+    {
+        'ThePrimeagen/harpoon', branch = 'harpoon2', dependencies = { 'nvim-lua/plenary.nvim' }, config = function()
+            local harpoon = require("harpoon")
+
+            harpoon:setup()
+
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+            -- Toggle previous & next buffers stored within Harpoon list
+            vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+            vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+        end
+    },
+
     -- C++
     { 'bfrg/vim-cpp-modern' },
     { 'p00f/clangd_extensions.nvim' },
